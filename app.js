f829bb32da9cc9ef1dbae2e246b917896b8bdeca
jpwebdev <jjpwebdev@gmail.com>
2018-01-15 12:44:56 -0500

    Supervisor mode created, supervisor can check the items available for sale and check low inventory

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-require('console.table');
 
 // Connection setup to MySQL
 const connection = mysql.createConnection({
@@ -14,7 +14,6 @@ const connection = mysql.createConnection({
 // Connect to MySQL DB
 connection.connect( err => {
     if (err) throw err;
-    showInventory();
     bamazon();
 });
 
@@ -27,22 +26,39 @@ const showInventory = () => {
         if (err) throw err;
         console.log("\n");
         console.table(res); // Display the table in the console
-        console.log("\n");
+        console.log("\n \n");
     });
 };
 
+// Checks for low inventory items (stock < 5 units)
+const lowInv = () => {
+    let sql = `SELECT * FROM products WHERE stock_quantity < 5`;
+
+    connection.query(sql, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        bamazon();
+    })
+    
+};
+
+const addInv = () => {
+
+}
+
 // Initiate with Customer rights
 const customer = () => {
+    showInventory();
     // Ask user the ID and quantity of the item they'd like to purchase
     inquirer.prompt(
         [
             {
                 name: "item",
-                message: "What is the ID of the item you'd like to buy"
+                message: "What is the ID of the item you'd like to purhcase"
             },
             {
                 name: "quantity",
-                message: "How many units would you like to buy?"
+                message: "How many units would you like to get?"
             }
         ]
     ).then( answer => {
@@ -62,13 +78,13 @@ const customer = () => {
             
             // If it there's enough items
             if (q > 0) {
-                console.log(`Your total is: $ ${userQ*p}.`);
-                console.log(`Thanks for your purchase, enjoy your ${prod}`);
                 // Update inventory using the user input
                 let sql = `UPDATE products SET stock_quantity = stock_quantity - ${userQ} WHERE item_id = ${userProdID}`;
                 connection.query(sql, (err, res) => {
                     if (err) throw err;
                     showInventory();
+                    console.log(`Your total is: $ ${userQ*p}.`);
+                    console.log(`Thanks for your purchase, enjoy your ${prod}`);
                     bamazon();
                 })
             }
@@ -80,9 +96,38 @@ const customer = () => {
     });
 }
 
+const manager = () => {
+    // Display options for the manager
+    inquirer.prompt(
+        {
+            name: "action",
+            type: "list",
+            message: "What would you like to do Mr. Manager?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+        }
+    ).then( answer => {
+        switch(answer.action) {
+            case "View Products for Sale":
+                showInventory();
+                manager();
+                break;
+            case "View Low Inventory":
+                lowInv();
+                break;
+            case "Add to Inventory":
+                addInv();
+                break;
+            case "Add New Product":
+                newProd();
+                break;
+            default:
+                console.log("Invalid!!!"); 
+        }
+    });
+}
+
 // Ask the user what options they'd like to see (customer, manager, supervisor)
 const bamazon = () => {
-    console.log("\n\n");
     // Prompt user the different options
     inquirer.prompt(
         {
